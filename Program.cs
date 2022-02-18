fa96a61f45720d5cdd533260fe4b2d3badced597
Burning <2250911301@qq.com>
2022-02-18 20:04:16 +0800

    Neusoft sign
    First version. Slide block can improve.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using SignSystem;
+using System.Timers;
+
+Console.WriteLine(DateTime.Now);
+Random random = new();
+int min = 0;
+int max = 0 ;
+
+
+System.Timers.Timer timer = new System.Timers.Timer();
+timer.Enabled = true;
+timer.Interval = 60000;//执行间隔时间,单位为毫秒;此时时间间隔为1分钟  
+timer.Start();
+timer.Elapsed += new System.Timers.ElapsedEventHandler(Sign);
+
+Console.ReadKey();
+
+
+void Sign(object? source, ElapsedEventArgs e)
+{
+    
+    if (DateTime.Now.Hour == 19 && DateTime.Now.Minute == 00)  //如果当前时间是10点30分
+    {
+        EveSign();
+    }
+    else if (DateTime.Now.Hour == 7 && DateTime.Now.Minute == 00)
+    {
+        MorningSign();
+    }
+    else
+    {
+        Console.WriteLine(DateTime.Now.ToString());
+
+    }
+}
+
+void MorningSign()
+{
+    Thread.Sleep(random.Next(min, max) * 60 * 1000);
+    SignUtils.Sign();
+    Console.WriteLine("早上打卡结束");
+}
+
+void EveSign()
+{
+    Thread.Sleep(new Random().Next(min, max) * 60 * 1000);
+    SignUtils.Sign();
+    Console.WriteLine("晚上打卡结束");
+}
---
+++ SignUtils.cs
@@ -0,0 +1,108 @@
+﻿using OpenCvSharp;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignSystem
+{
+    internal class SignUtils
+    {
+        public static void TestOpenCv()
+        {
+            Mat src = new Mat("D:\\vs2022\\Project\\cvsharp_check\\lenna.jpg", ImreadModes.Grayscale);//小写的scale，不然报错
+            Mat dst = new Mat();
+            Mat dst1 = new Mat();
+
+            Cv2.Canny(src, dst, 50, 200);
+            Cv2.Add(src, dst, dst1);
+            using (new Window("src image", src))
+            using (new Window("dst image", dst))
+            using (new Window("dst1 image", dst1))
+            {
+                Cv2.WaitKey();
+            }
+        }
+       
+
+
+        public static void Sign()
+        {
+            Console.WriteLine("Hello World!");
+
+            IWebDriver wd = new ChromeDriver();
+            wd.Navigate().GoToUrl("http://kq.neusoft.com/");
+            IWindow window = wd.Manage().Window;
+            window.Maximize();
+            Thread.Sleep(3000);
+            wd.FindElement(By.ClassName("userName")).SendKeys("tengyb");
+            wd.FindElement(By.ClassName("password")).SendKeys("18345093167ASdgy123");
+            int distance = 85;
+            Random random = new Random();
+            int minoffset = 20;
+            int maxoffset = 85;
+
+            int times = 0;
+            while (true)
+            {
+
+                //这是滑块
+                var slide = wd.FindElement(By.ClassName("ui-slider-btn"));
+                Actions action = new Actions(wd);
+                //点击并按住滑块元素
+                action.ClickAndHold(slide);
+                action.MoveByOffset(distance*2, 0);
+                Thread.Sleep(1000);
+                string alert;
+
+         
+         
+                try
+                {
+                    alert = wd.FindElement(By.ClassName("ui-slider-text")).Text;
+                    Console.WriteLine(alert);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("发生异常:" + e.ToString());
+                    alert = "";
+                }
+
+
+                if (alert.Contains("验证成功"))
+                {
+                    Console.WriteLine("滑块验证成功!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("滑块验证失败!");
+                    action.Release().Perform();
+                    distance = random.Next(minoffset, maxoffset);
+                    Thread.Sleep(1000);
+                    wd.SwitchTo().DefaultContent();
+                }
+                Thread.Sleep(1000);
+
+            }
+            wd.FindElement(By.Id("loginButton")).Click();
+            Thread.Sleep(3000);
+
+            //Thread.Sleep(3*60*1000);
+            string js_sign = "javascript:document.attendanceForm.submit();";
+            ((ChromeDriver)wd).ExecuteScript(js_sign, null);
+            Thread.Sleep(2000);
+            //调用js
+            string js_exit = "javascript:exitAttendance();";
+            ((ChromeDriver)wd).ExecuteScript(js_exit, null);
+            Thread.Sleep(3000);
+            wd.Quit();
+        }
+
+    }
+}
