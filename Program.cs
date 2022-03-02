6889fc0249edd7b7b49a72b088005631bec9586c
Burning <2250911301@qq.com>
2022-03-02 22:52:53 +0800

    Add code to deal image match.
    Add Monitor to Get images.

--- Program.cs
+++ Program.cs
@@ -9,149 +9,64 @@ SignUtils.User32.ShowWindow(ParenthWnd, 0);//隐藏本dos窗体, 0: 后台执行
 
 
 //需要考虑法定节假日和周末
-Console.WriteLine("今年是：" + DateTime.Now.Year + "年");
+Console.WriteLine("今年是：" + DateTime.Now.Year);
 List<string> list = new List<string>();
 
 //sk.Factory.StartNew(() => SignUtils.TestOpenCv("C:/Users/burning/Pictures/20201205234844361.png"));
 Random random = new();
 int min = 0;
 int max = 120;
+SignUtils su = new();
 
-/*SignProxy signProxy = new();
-Task task = new(() => { 
-    signProxy.StartProxyServer();
-    signProxy.SetProxyPort();
-    Console.ReadKey();
-    Console.WriteLine("close proxy");
-    signProxy.StopProxyServer();
-});
-task.Start();*/
 
+//signProxy.StopProxyServer();
+
+
+//等待任务完成
+/*task.GetAwaiter().OnCompleted(() =>
+{
+    Console.WriteLine("打卡完成");
+   // signProxy.StopProxyServer();
+});*/
 //定时器相关代码
 System.Timers.Timer timer = new System.Timers.Timer();
 timer.Enabled = true;
 timer.Interval = 60000;//执行间隔时间,单位为毫秒;此时时间间隔为1分钟  
 timer.Start();
 timer.Elapsed += new System.Timers.ElapsedEventHandler(SignFilter);
-
 Console.ReadKey();
-
 
 void SignFilter(object? source, ElapsedEventArgs e)
 {
-    if (DateTime.Now.Month == 4)
-    {
-        if (DateTime.Now.Day >=  2 && DateTime.Now.Day <= 5)
-        {
-            Console.WriteLine("清明假期，正常打卡");
-            Sign();
-        }
-        //周日
-        else if (DateTime.Now.Day == 24)
-        {
-            Console.WriteLine("劳动节调休，周日正常上班");
-            Sign();
-        }
-        //五一假期
-        else if (DateTime.Now.Day == 30 )
-        {
-            Console.WriteLine("劳动节假期，正常打卡");
-            Sign();
-        }
-        else
-        {
-            SignNormal();
-        }
-    }
-    //劳动节
-    else if (DateTime.Now.Month == 5)
-    {
-        Console.WriteLine(DateTime.Now);
-        if (DateTime.Now.Day >= 1 && DateTime.Now.Day <= 4) {
-            Sign();
-        }
-        else if (DateTime.Now.Day == 7)
-        {
-            Sign();
-        } 
-        else
-        {
-            SignNormal();
-        }
-    }
-    //端午节
-    else if (DateTime.Now.Month == 6)
-    {
-        if (DateTime.Now.Day >= 3 && DateTime.Now.Day <= 5)
-        {
-            Sign();
-        } else
-        {
-            SignNormal();
-        }
-
-    }
-
-    //中秋节
-    else if (DateTime.Now.Month == 9)
-    {
-        if (DateTime.Now.Day >= 10 && DateTime.Now.Day <= 12)
-        {
-            Sign();
-        }
-        else
-        {
-            SignNormal();
-        }
-
-    }
-
-    //国庆节
-    else if (DateTime.Now.Month == 10)
-    {
-        if (DateTime.Now.Day >= 2 && DateTime.Now.Day <= 9)
-        {
-            Sign();
-        }
-        else
-        {
-            SignNormal();
-        }
-    } else
-    {
-        SignNormal();
-    }
-}
-
-void SignNormal()
-{
-    if (DateTime.Now.DayOfWeek.ToString().Equals("Saturday") || (DateTime.Now.DayOfWeek.ToString().Equals("Sunday")))
-    {
-        Console.WriteLine("今天是" + DateTime.Now.DayOfWeek + "时间是:" + DateTime.Now);
-    } else
-    {
-        Sign();
-    }
-}
-
-void Sign()
-{
-
     if (DateTime.Now.Hour == 7 && DateTime.Now.Minute == 00)  //如果当前时间是20点15分
     {
         Thread.Sleep(random.Next(min, max) * 60 * 1000);
-        SignUtils.Sign();
+        su.StartProxyServer();
+        su.SetProxyPort();
+        var task = Task.Run(() =>
+        {
+            su.Sign();
+        });
+        task.Wait();
+        su.StopProxyServer();
         Console.WriteLine("早上打卡结束");
     }
-    else if (DateTime.Now.Hour == 19 && DateTime.Now.Minute == 48)
+    else if (DateTime.Now.Hour == 19 && DateTime.Now.Minute == 00)
     {
         Thread.Sleep(random.Next(min, max) * 60 * 1000);
-        SignUtils.Sign();
+        su.StartProxyServer();
+        su.SetProxyPort();
+        var task = Task.Run(() =>
+        {
+            su.Sign();
+        });
+        task.Wait();
+        su.StopProxyServer();
         Console.WriteLine("晚上打卡结束");
     }
     else
     {
-        Console.WriteLine(DateTime.Now);
+        Console.WriteLine("当前时间是" + DateTime.Now);
     }
 
 }
