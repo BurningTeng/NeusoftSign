df7487f40f4911800226608e1a3f4ba4ebe0fa72
Burning <2250911301@qq.com>
2022-03-13 17:39:14 +0800

    Remove unused code

--- Program.cs
+++ Program.cs
@@ -2,30 +2,15 @@
 using System.Diagnostics;
 using System.Timers;
 
-
-/*Console.Title = "SignSystem";
-IntPtr ParenthWnd = new IntPtr(0);
-ParenthWnd =SignUtils.User32.FindWindow(null, "SignSystem");
-SignUtils.User32.ShowWindow(ParenthWnd, 0);//隐藏本dos窗体, 0: 后台执行；1:正常启动；2:最小化到任务栏；3:最大化*/
-
-
 //需要考虑法定节假日和周末
 Console.WriteLine("今年是：" + DateTime.Now.Year);
 Console.WriteLine("当前进程号是:" + Process.GetCurrentProcess() + ", 线程号是:" + Thread.CurrentThread.ManagedThreadId);
-List<string> list = new List<string>();
 
-//sk.Factory.StartNew(() => SignUtils.TestOpenCv("C:/Users/burning/Pictures/20201205234844361.png"));
 Random random = new();
 int min = 0;
 int max = 120;
 SignUtils su = new();
 
-//等待任务完成
-/*task.GetAwaiter().OnCompleted(() =>
-{
-    Console.WriteLine("打卡完成");
-});*/
-//定时器相关代码
 System.Timers.Timer timer = new System.Timers.Timer();
 timer.Enabled = true;
 timer.Interval = 60000;//执行间隔时间,单位为毫秒;此时时间间隔为1分钟  
